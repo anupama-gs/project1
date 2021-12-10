bcdaca295ca57a50cf4eb5a7f1cccc28905e25d9
anupama-gs <anupama.g.srinivasan@gmail.com>
2021-12-10 10:47:58 +0530

    tox,flake added

---
+++ tests/test_config.py
@@ -0,0 +1,26 @@
+
+from _pytest.python_api import raises
+import pytest
+
+class NotInRange (Exception) :
+    def __init__(self, message = "Value not in range ") :
+        self.message = message
+        super().__init__(self.message)
+
+def test_generic() :
+    a = 2
+    b = 2
+    assert a==b
+
+def test_generic1() :
+    print ("generic")
+    a = 2
+    b = 21
+    assert a!=b
+
+def test_exception() :
+    a = 15
+    print ("in exception")
+    with pytest.raises (ValueError) :
+        if a not in range(10, 20) :
+            raise NotInRange
